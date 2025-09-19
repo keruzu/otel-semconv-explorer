6fa3e38f63352676525e4ecfd6e28f228b75a808
Kearney, Kells <keruzu@gmail.com>
2025-09-19 19:04:51 -0400

    Refactor into a separate Kuzu class for persistence

--- read_yaml_from_disk.py
+++ read_yaml_from_disk.py
@@ -1,5 +1,4 @@
 import json
-import csv
 import logging
 from pathlib import Path
 
@@ -7,7 +6,6 @@ import yaml
 from pythonjsonlogger.json import JsonFormatter
 import kuzu
 
-# --- Logging Configuration ---
 handler = logging.StreamHandler()
 handler.setFormatter(JsonFormatter())
 logging.basicConfig(level=logging.INFO, handlers=[handler])
@@ -15,7 +13,6 @@ logging.basicConfig(level=logging.INFO, handlers=[handler])
 log = logging.getLogger()
 
 
-# --- YAML Processing Function ---
 class SemanticConventions(object):
     def __init__(self, log=None):
         self.path_start = -1
@@ -23,21 +20,26 @@ class SemanticConventions(object):
         self.reset()
 
     def reset(self):
-        self.relations = dict(has_attribute={}, has_event={}, AssociatedWith={})
+        """
+        Reset all semantic convention data
+        """
+        # Use the Cypher table names as indices, so it's easier to relate things
+        self.relations = dict(HasAttribute={}, HasEvent={}, AssociatedWith={})
         self.nodes = dict(Metric={}, Entity={}, Span={}, AttributeGroup={}, Event={}, Attribute={})
 
     def nodetype2node(self, section: dict) -> str:
         """
         Map OpenTelemetry 'type' field to our Cypher node table name
-        :return:
+
+        :return: table name
         """
         mappings = dict(metric='Metric', entity='Entity', span='Span', attribute_group='AttributeGroup', event='Event')
         node_type = section.get('type')
         return mappings.get(node_type)
 
-    def from_dir(self, base_path: str):
+    def import_conventions_from_dir(self, base_path: str):
         """
-        Reads all YAML files in a given directory and its subdirectories using pathlib.
+        Reads all semantic convention YAML files in a given directory and its subdirectories.
 
         Args:
             base_path (str): The root directory to start the search from.
@@ -64,8 +66,7 @@ class SemanticConventions(object):
         """
         Process the 'groups' entry from a YAML semantic convention
 
-        :param yaml_data:
-        :return:
+        :param yaml_data: semantic convention 'groups' entry
         """
         for section in yaml_data.get('groups', []):
             node_type = self.nodetype2node(section)
@@ -86,7 +87,15 @@ class SemanticConventions(object):
                 self.log.error("Unknown semantic convention", extra=dict(data=section, node_type=node_type))
 
     def relate2attribute(self, node_type: str, node: str, attributes: list):
-        rels = self.relations['has_attribute'].setdefault(node_type, [])
+        """
+        Add a relation from a node type and node to an attribute.
+        An attribute can either be a reference, or an object to be persisted.
+
+        :param node_type: node table name
+        :param node: name of the node
+        :param attributes: list of attribute entries (dictionaries)
+        """
+        rels = self.relations['HasAttribute'].setdefault(node_type, [])
         for data in attributes:
             edge_info = {'from': node}
             if 'ref' in data:
@@ -112,18 +121,36 @@ class SemanticConventions(object):
             rels.append(edge_info)
 
     def relate2associated_entity(self, node_type: str, node: str, entities: list):
+        """
+        Metrics can have associated entities
+
+        :param node_type: node table name
+        :param node: name of the node
+        :param entities: list of entity entries (dictionaries)
+        """
         rels = self.relations['AssociatedWith'].setdefault(node_type, [])
         for entity in entities:
             # Evil hack: supposed to search entity table by 'name' or by 'id', which can be different
             # Choose the id, assume that the difference is that the id name starts with 'entity.'
             if not entity.startswith('entity'):
                 entity = 'entity.' + entity
-            rels.append((node, entity))
+            edge_info = {'from': node, 'to': entity}
+            rels.append(edge_info)
 
     def relate2event(self, node_type: str, node: str, events: list):
-        rels = self.relations['has_event'].setdefault(node_type, [])
+        """
+        Spans can have associated events
+
+        :param node_type: node table name
+        :param node: name of the node
+        :param events: list of events entries (dictionaries)
+        """
+        rels = self.relations['HasEvent'].setdefault(node_type, [])
         for event_name in events:
-            rels.append((node, event_name))
+            if not event_name.startswith('event'):
+                event_name = 'event.' + event_name
+            edge_info = {'from': node, 'to': event_name}
+            rels.append(edge_info)
 
     def add_attribute(self, attribute: dict):
         key = attribute['id']
@@ -152,77 +179,61 @@ class SemanticConventions(object):
         return key_name
 
 
-def save_rel_data_csv(rel_type: str, relations: list) -> str:
-    filename = f'rel_{rel_type}.csv'
-    with open(filename, 'w') as fd:
-        writer = csv.writer(fd, dialect='unix')
-        writer.writerows(relations)
-    return filename
+class PersistenceKuzu(SemanticConventions):
+    relation2node = {
+        'HasAttribute': 'Attribute',
+        'HasEvent': 'Event',
+        'AssociatedWith': 'Entity',
+    }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.conn = None
 
-def save_rel_data_json(node_type: str, rel_type: str, relations: list) -> str:
-    filename = f'rel_{node_type}_{rel_type}.json'
-    with open(filename, 'w') as fd:
-        json.dump(relations, fd)
-    return filename
+    def create_db(self, path: str = 'db/semantic_conventions.kuzu',
+                  model_file: str = 'kuzu_data_model.cypher'):
+        db = kuzu.Database(path)
+        self.conn = kuzu.Connection(db)
+        if model_file:
+            with open(model_file) as fd:
+                model_data = fd.read()
+            statements = model_data.split(';')
+            for statement in statements:
+                if statement.strip():
+                    self.execute(statement)
 
+    def persist_nodes(self):
+        for node_type, data in self.nodes.items():
+            filename = node_type + 's.json'
+            self.save_import_file(filename, list(data.values()))
+            statement = f"COPY {node_type} FROM '{filename}' (ignore_errors=true)"
+            self.execute(statement)
 
-def save_node_data_json(node_type: str, nodes: list) -> str:
-    filename = node_type + 's.json'
-    with open(filename, 'w') as fd:
-        json.dump(nodes, fd)
-    return filename
+    def persist_relations(self):
+        for rel_name, rel_data in self.relations.items():
+            rel_endpoint = self.relation2node.get(rel_name)
+            for node_type, relations in rel_data.items():
+                filename = f'rel_{node_type}_{rel_name}.json'
+                self.save_import_file(filename, relations)
+                statement = f"COPY {rel_name} FROM '{filename}' (from='{node_type}', to='{rel_endpoint}')"
+                self.execute(statement)
 
+    def save_import_file(self, filename: str, nodes: list):
+        with open(filename, 'w') as fd:
+            json.dump(nodes, fd)
 
-def save_node_data_kuzu(conn: object, table: str, filename: str):
-    name2type = dict(attribute_group='AttributeGroup', entity='Entity',
-                     metric='Metric', event='Event', span='Span', attribute='Attribute')
-    statement = f"COPY {table} FROM '{filename}' (ignore_errors=true)"
-    conn.execute(statement)
-
-
-def create_db_conn(path: str = 'db/semantic_conventions.kuzu',
-                   model_file: str = 'kuzu_data_model.cypher') -> object:
-    db = kuzu.Database(path)
-    conn = kuzu.Connection(db)
-    if model_file:
-        with open(model_file) as fd:
-            model_data = fd.read()
-        statements = model_data.split(';')
-        for statement in statements:
-            if statement.strip():
-                conn.execute(statement)
-    return conn
+    def execute(self, statement: str):
+        try:
+            self.conn.execute(statement)
+        except Exception as ex:
+            print(f"{ex} {statement}")
 
 
 if __name__ == '__main__':
     START_PATH = '../semantic-conventions/model'
 
-    conventions = SemanticConventions(log)
-    conventions.from_dir(START_PATH)
-
-    conn = create_db_conn()
-    for node_type, data in conventions.nodes.items():
-        filename = save_node_data_json(node_type, list(data.values()))
-        save_node_data_kuzu(conn, node_type, filename)
-
-    # statement = 'MATCH (n: Attribute) RETURN n;'
-    # result = conn.execute(statement)
-    for node_type, relations in conventions.relations['has_attribute'].items():
-        filename = save_rel_data_json(node_type, 'attribute', relations)
-        statement = f"COPY HasAttribute FROM '{filename}' (from='{node_type}', to='Attribute')"
-        try:
-            conn.execute(statement)
-        except Exception as ex:
-            print(f"{ex} {statement}")
-            import pdb;
-
-            pdb.set_trace()
-
-    for node_type, relations in conventions.relations['AssociatedWith'].items():
-        filename = save_rel_data_csv(node_type + '_entity_association', relations)
-        statement = f"COPY AssociatedWith FROM '{filename}' (from='{node_type}', to='Entity')"
-        try:
-            conn.execute(statement)
-        except Exception as ex:
-            print(f"{ex} {statement}")
+    conventions = PersistenceKuzu(log)
+    conventions.import_conventions_from_dir(START_PATH)
+    conventions.create_db()
+    conventions.persist_nodes()
+    conventions.persist_relations()
